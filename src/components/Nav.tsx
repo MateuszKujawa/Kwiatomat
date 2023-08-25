aafc8f9fea97f7174bb6abe4fd6bb5092b56ca3a
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-08-25 14:36:50 +0200

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -20,7 +20,7 @@ const Nav = () => {
         </div>
 
         <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden">
-          <ion-icon name={open ? 'close':'menu'}></ion-icon>
+          {/* <ion-icon name={open ? 'close':'menu'}></ion-icon> */}
         </div>
 
         <ul
