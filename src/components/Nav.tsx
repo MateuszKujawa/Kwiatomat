48e480624f278fe5c3667eebbd3addf56ac623ac
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-08-25 13:37:43 +0200

    ,

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -19,17 +19,13 @@ const Nav = () => {
           </p>
         </div>
 
-        <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden"
-        >
-          MENU
-          {/* <ion-icon name={open ? "close" : "menu"}></ion-icon> */}
+        <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden">
+          <ion-icon name={open ? 'close':'menu'}></ion-icon>
         </div>
 
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-10 " : "top-[-480px]"
+            open ? 'top-10 ' : 'top-[-480px]'
           }`}
         >
           {Links.map((link) => (
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,3 +4,4 @@ declare module '*.jpg' {
   const fileName: string;
   export default fileName;
 }
+
